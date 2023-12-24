7b42f950108f9cb95656c6874b29641f567dc260
HARSHITH CHINTAKINDI <127752210+harshith1817@users.noreply.github.com>
2023-12-24 09:58:27 +0530

    Week10c

--- DecisionTree.r
+++ DecisionTree.r
@@ -1,7 +1,15 @@
+# Load the party package. It will automatically load other
+# dependent packages.
 library(party)
+# Create the input data frame.
 input.dat <- readingSkills[c(1:105),]
+# Give the chart file a name.
 png(file = "decision_tree.png")
-output.tree <- ctree( nativeSpeaker ~ age + shoeSize + score, data =
-                        input.dat)
+# Create the tree.
+  output.tree <- ctree(
+  nativeSpeaker ~ age + shoeSize + score, 
+  data = input.dat)
+# Plot the tree.
 plot(output.tree)
+# Save the file.
 dev.off()
