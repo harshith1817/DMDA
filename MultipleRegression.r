5bdddabb186dafdc505c53107980e5f86b62f1e1
HARSHITH CHINTAKINDI <127752210+harshith1817@users.noreply.github.com>
2023-12-24 09:46:00 +0530

    Week9b

---
+++ MultipleRegression.r
@@ -0,0 +1,11 @@
+input<-mtcars[,c("mpg","disp","hp","wt")]
+model<-lm(mpg~disp+hp+wt,data=input)
+print(model)
+a<-coef(model)[1]
+print(a)
+xdisp<-coef(model)[2]
+xhp<-coef(model)[3]
+xwt<-coef(model)[4]
+print(xdisp)
+print(xhp)
+print(xwt)
