3a9068cde445656e0516e9b2918e471035cf06e7
HARSHITH CHINTAKINDI <127752210+harshith1817@users.noreply.github.com>
2023-12-24 10:00:21 +0530

    Week9a

--- LinearRegression.r
+++ LinearRegression.r
@@ -1,8 +1,12 @@
 #Creating input vector for lm() function  
-x <- c(141, 134, 178, 156, 108, 116, 119, 143, 162, 130)  
-y <- c(62, 85, 56, 21, 47, 17, 76, 92, 62, 58)  
-relationship_model<- lm(y~x)  
-
-# Plotting the chart.  
-plot(y,x,col = "blue",main = "Height and Weight Regression",abline(lm(x~y)),
-     cex = 1,pch = 16,xlab = "Weight in Kg",ylab = "Height in cm")
+    x <- c(141, 134, 178, 156, 108, 116, 119, 143, 162, 130)  
+    y <- c(62, 85, 56, 21, 47, 17, 76, 92, 62, 58)  
+    relationship_model<- lm(y~x)  
+    # Giving a name to the chart file.  
+    png(file = "linear_regression.png")  
+    # Plotting the chart.  
+    plot(y,x,col = "red",main = "Height and Weight Regression",
+	abline(lm(x~y)),cex = 1.3,pch = 16,xlab = "Weight in Kg",
+	ylab = "Height in cm")  
+    # Saving the file.  
+    dev.off()  
