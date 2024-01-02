d182bf86f70e4d0f2a0395881b7b1fd47e71a797
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2024-01-02 23:23:51 +0530

    Week10b

--- NonLinearLeastSquare.r
+++ NonLinearLeastSquare.r
@@ -3,9 +3,9 @@ df <- data.frame(hours=c(1, 2, 4, 5, 5, 6, 6, 7, 8, 10, 11, 11, 12, 12, 14),
                  score=c(64, 66, 76, 73, 74, 81, 83, 82, 80, 88, 84, 82, 91, 93, 89))
 
 #view first six rows of data frame
-head(df)
+print(head(df))
 #use method of least squares to fit regression line
 model <- lm(score ~ hours, data=df)
 
 #view regression model summary
-summary(model)
+print(summary(model))
