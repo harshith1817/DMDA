af53800b5a5c9795fb7666f200e8c0e39e93404e
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2024-01-02 23:38:44 +0530

    Week11b

--- BinomialDistribution.r
+++ BinomialDistribution.r
@@ -1,18 +1,26 @@
-dbinom(3, size = 13, prob = 1 / 6) 
-probabilities <- dbinom(x = c(0:10), size = 10, prob = 1 / 6) 
-data.frame(x, probs) 
-plot(0:10, probabilities, type = "l") 
+# Create a sample of 50 numbers which are incremented by 1.
+x <- seq(0,50,by = 1)
+# Create the binomial distribution.
+y <- dbinom(x,50,0.5)
+# Give the chart file a name.
+png(file = "dbinom.png")
+# Plot the graph for this sample.
+plot(x,y)
+# Save the file.
+dev.off()
 
 
-pbinom(3, size = 13, prob = 1 / 6) 
-plot(0:10, pbinom(0:10, size = 10, prob = 1 / 6), type = "l") 
+# Probability of getting 26 or less heads from a 51 tosses of a coin.
+x <- pbinom(26,51,0.5)
+print(x)
 
 
-qbinom(0.8419226, size = 13, prob = 1 / 6) 
-x <- seq(0, 1, by = 0.1) 
-y <- qbinom(x, size = 13, prob = 1 / 6) 
-plot(x, y, type = 'l') 
+# How many heads will have a probability of 0.25 will come out when a coin
+# is tossed 51 times.
+x <- qbinom(0.25,51,1/2)
+print(x)
 
 
-rbinom(8, size = 13, prob = 1 / 6) 
-hist(rbinom(8, size = 13, prob = 1 / 6)) 
+# Find 8 random values from a sample of 150 with probability of 0.4.
+x <- rbinom(8,150,.4)
+print(x)
